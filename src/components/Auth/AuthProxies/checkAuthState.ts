9c14121b8c772396a7fa40e511e7f0a7e8420556
Zheng Hanming <hanming2033@gmail.com>
2018-07-17 06:38:03 +0800

    remove get session because currentuser has session info

--- src/components/Auth/AuthProxies/checkAuthState.ts
+++ src/components/Auth/AuthProxies/checkAuthState.ts
@@ -1,10 +1,9 @@
 import { Auth } from 'aws-amplify'
-import { ICognitoUser, ICognitoUserSession } from './AuthTypes'
+import { ICognitoUser } from './AuthTypes'
 
 interface ICheckAuthStateResult {
   data?: {
     user: ICognitoUser
-    session?: ICognitoUserSession
   }
   error?: string
 }
@@ -13,15 +12,9 @@ export const checkAuthState = async (): Promise<ICheckAuthStateResult> => {
   try {
     // get current user
     const user = await Auth.currentAuthenticatedUser()
-    // get current session
-    const session = await Auth.currentSession()
 
-    console.log('Proxy checkAuthState Success ', user, session)
-    return { data: { user, session } }
-    // !removed
-    // check if current user is a verified user
-    // const verificationDetail = await verifyUser(user)
-    // if (JS.isEmpty(verificationDetail.unverified)) return { data: { user } }
+    console.log('Proxy checkAuthState Success ', user)
+    return { data: { user } }
   } catch (error) {
     console.log('Proxy checkAuthState Fail : ', error)
     return { error }
