a10a86aa40f582ab9a2954fab8d902599bd37b4f
Zheng Hanming <hanming2033@gmail.com>
2018-07-17 06:37:35 +0800

    removed comments

--- src/components/Auth/AuthProxies/verifyUser.ts
+++ src/components/Auth/AuthProxies/verifyUser.ts
@@ -1,29 +1,7 @@
 import { Auth } from 'aws-amplify'
 
 export const verifyUser = async (user: any) => {
-  // const res: GetLocalStatesQuery | null = client.readQuery({ query: GET_LOCAL_STATES })
-  // if (!res || !res.auth) return { verified: {}, unverified: {} }
   const verification = await Auth.verifiedContact(user)
   console.log('VerifyUser Post-Verification Result : ', verification)
-  // const newData: GetLocalStatesQuery = {
-  //   ...res,
-  //   auth: {
-  //     ...res.auth
-  //   }
-  // }
-
-  // if (!JS.isEmpty(verification.verified)) {
-  //   if (newData.auth) {
-  //     newData.auth.isAuthenticated = true
-  //   }
-  // } else {
-  //   if (newData.auth) {
-  //     newData.auth.isAuthenticated = false
-  //   }
-  // }
-  // client.writeQuery({
-  //   query: GET_LOCAL_STATES,
-  //   data: newData
-  // })
   return verification
 }
